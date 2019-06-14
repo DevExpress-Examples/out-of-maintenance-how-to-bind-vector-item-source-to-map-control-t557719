918c73be588c592d99e0080c9cfa4cf1a0b7d111
DevExpressExampleBot <fakeEmail>
2019-06-14 14:13:27 +0300

    Source auto update [skip ci]

--- VB/MapControl_Lesson3/MainPage.xaml.vb
+++ VB/MapControl_Lesson3/MainPage.xaml.vb
@@ -9,53 +9,53 @@ Imports Windows.UI.Xaml
 Imports Windows.UI.Xaml.Controls
 
 Namespace MapControl_Lesson3
-    Public NotInheritable Partial Class MainPage
-        Inherits Page
+	Public NotInheritable Partial Class MainPage
+		Inherits Page
 
-        Private Const filepath As String = "Assets\ships.xml"
+		Private Const filepath As String = "Assets\ships.xml"
 
+'INSTANT VB NOTE: The field shipwrecks was renamed since Visual Basic does not allow fields to have the same name as other class members:
+		Private shipwrecks_Renamed As New ObservableCollection(Of MapItem)()
+		Public ReadOnly Property Shipwrecks() As ObservableCollection(Of MapItem)
+			Get
+				Return shipwrecks_Renamed
+			End Get
+		End Property
 
-        Private shipwrecks_Renamed As New ObservableCollection(Of MapItem)()
-        Public ReadOnly Property Shipwrecks() As ObservableCollection(Of MapItem)
-            Get
-                Return shipwrecks_Renamed
-            End Get
-        End Property
+		Public Sub New()
+			Me.InitializeComponent()
+			LoadShipwrecks()
+			Me.DataContext = Me
+		End Sub
 
-        Public Sub New()
-            Me.InitializeComponent()
-            LoadShipwrecks()
-            Me.DataContext = Me
-        End Sub
+		Private Sub LoadShipwrecks()
+			Dim xmlFilepath As String = Path.Combine(Package.Current.InstalledLocation.Path, filepath)
+			Dim document As XDocument = XDocument.Load(xmlFilepath)
+			For Each element As XElement In document.Element("Ships").Elements()
+				Dim location As New GeoPoint() With {
+					.Latitude = Convert.ToDouble(element.Element("Latitude").Value, CultureInfo.InvariantCulture),
+					.Longitude = Convert.ToDouble(element.Element("Longitude").Value, CultureInfo.InvariantCulture)
+				}
+				Dim e As New MapCustomElement() With {
+					.Location = location,
+					.ContentTemplate = TryCast(Me.Resources("itemTemplate"), DataTemplate)
+				}
+				e.Attributes.Add(New MapItemAttribute() With {
+					.Name = "Description",
+					.Value = element.Element("Description").Value
+				})
+				e.Attributes.Add(New MapItemAttribute() With {
+					.Name = "Name",
+					.Value = element.Element("Name").Value
+				})
+				e.Attributes.Add(New MapItemAttribute() With {
+					.Name = "Year",
+					.Value = element.Element("Year").Value
+				})
 
-        Private Sub LoadShipwrecks()
-            Dim xmlFilepath As String = Path.Combine(Package.Current.InstalledLocation.Path, filepath)
-            Dim document As XDocument = XDocument.Load(xmlFilepath)
-            For Each element As XElement In document.Element("Ships").Elements()
-                Dim location As New GeoPoint() With { _
-                    .Latitude = Convert.ToDouble(element.Element("Latitude").Value, CultureInfo.InvariantCulture), _
-                    .Longitude = Convert.ToDouble(element.Element("Longitude").Value, CultureInfo.InvariantCulture) _
-                }
-                Dim e As New MapCustomElement() With { _
-                    .Location = location, _
-                    .ContentTemplate = TryCast(Me.Resources("itemTemplate"), DataTemplate) _
-                }
-                e.Attributes.Add(New MapItemAttribute() With { _
-                    .Name = "Description", _
-                    .Value = element.Element("Description").Value _
-                })
-                e.Attributes.Add(New MapItemAttribute() With { _
-                    .Name = "Name", _
-                    .Value = element.Element("Name").Value _
-                })
-                e.Attributes.Add(New MapItemAttribute() With { _
-                    .Name = "Year", _
-                    .Value = element.Element("Year").Value _
-                })
-
-                shipwrecks_Renamed.Add(e)
-            Next element
-        End Sub
-    End Class
+				shipwrecks_Renamed.Add(e)
+			Next element
+		End Sub
+	End Class
 
 End Namespace
